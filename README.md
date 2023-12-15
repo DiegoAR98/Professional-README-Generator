a529bbdeea1026f92eafd6c020dc7d350d1fd3fd
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2023-12-15 09:18:11 -0500

    Added README to the project

---
+++ README.md
@@ -0,0 +1,40 @@
+# Professional Readme Generator
+
+![License](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+Professional Readme Generator is a command-line application designed to create well-structured and detailed README files for software projects. It guides users through a series of prompts to gather essential project information and automatically formats it into a professional README document.
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+To install the application, clone the repository to your local machine using git clone https://github.com/DiegoAR98/Professional-Readme-Generator.git. Then, navigate to the project directory and run npm install to install all necessary dependencies.
+
+## Usage
+
+After installation, run the application by executing node index.js in your terminal. Follow the interactive prompts to input your project details. Once all prompts are answered, a README.md file will be generated in the project directory
+
+## License
+
+This project is licensed under the MIT license.
+
+## Contributing
+
+Contributions to the Professional Readme Generator are welcome! Please fork the repository and submit pull requests for any enhancements. Ensure that your code adheres to the existing style and testing guidelines. For major changes, please open an issue first to discuss what you would like to change.
+
+## Tests
+
+o run tests, ensure that all development dependencies are installed by running npm install. Then execute npm test to run the test suite. The test suite includes unit tests to verify the functionality of the README generation process.
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at diegoaraujolive@hotmail.com. You can find more of my work at [DiegoAR98](https://github.com/DiegoAR98/).
--- index.js
+++ index.js
@@ -1,13 +1,81 @@
-// TODO: Include packages needed for this application
+//Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//Create an array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Enter contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter test instructions:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ];
+  
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error('Error writing file: ', err);
+    } else {
+      console.log(`${fileName} was created successfully.`);
+    }
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+//Create a function to initialize app
+function init() {
+  inquirer.prompt(questions)
+    .then((answers) => {
+      const markdownContent = generateMarkdown(answers);
+      writeToFile('README.md', markdownContent);
+    })
+    .catch((error) => {
+      console.error('An error occurred: ', error);
+    });
+}
 
 // Function call to initialize app
 init();
